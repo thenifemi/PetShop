a2ee1e5564781f92a492e63a8ca365258996b98c
Nifemi <thenifemi@gmail.com>
2020-03-08 20:21:07 -0300

    half of UI

--- lib/screens/register_screens/registration_screen.dart
+++ lib/screens/register_screens/registration_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:mollet/utils/colors.dart';
 import 'package:mollet/utils/strings.dart';
 
@@ -11,20 +10,27 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final formKey = GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  String _phoneNumber;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MColors.primaryWhiteSmoke,
-      body: Container(
-        height: double.infinity,
-        color: MColors.primaryWhiteSmoke,
+      body: SingleChildScrollView(
+        // height: double.infinity,
+        // color: MColors.primaryWhiteSmoke,
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(30.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.only(top: 60.0),
+                padding: const EdgeInsets.only(top: 75.0),
                 child: Text(
                   Strings.registraionTitle,
                   style: TextStyle(
@@ -58,15 +64,152 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                           "Sign in!",
                           style: TextStyle(
                             fontSize: 17.0,
-                            color: Colors.blue,
+                            color: MColors.primaryPurple,
                           ),
                           textAlign: TextAlign.start,
                         )),
                   ),
-
-                  //FORM
-                  
                 ],
+              ),
+
+              SizedBox(
+                height: 20.0,
+              ),
+
+              //FORM
+              Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "Name",
+                            style: TextStyle(color: MColors.textGrey),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "e.g Remiola",
+                              labelStyle: TextStyle(fontSize: 16.0),
+                              contentPadding: new EdgeInsets.symmetric( horizontal: 25.0),
+                              fillColor: MColors.primaryWhite,
+                              filled: true,
+                              hasFloatingPlaceholder: false,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.white, width: 0.0),
+                              ),
+                              focusedBorder: InputBorder.none,
+                            ),
+                            style: TextStyle(fontSize: 20.0, color: MColors.textDark),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "Email",
+                            style: TextStyle(color: MColors.textGrey),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: TextFormField(
+                            validator: (val) =>
+                                !val.contains("@") ? 'Invalid Email' : null,
+                            onSaved: (val) => _email = val,
+                            decoration: InputDecoration(
+                                labelText: "e.g Remiola2034@gmail.com",
+                                contentPadding: new EdgeInsets.symmetric(
+                                    vertical: 13.0, horizontal: 10.0),
+                                fillColor: MColors.primaryWhite,
+                                hasFloatingPlaceholder: false,
+                                filled: true,
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(color: MColors.textGrey),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: TextFormField(
+                            validator: (val) => val.length < 6
+                                ? 'Password not strong enough'
+                                : null,
+                            onSaved: (val) => _password = val,
+                            decoration: InputDecoration(
+                                labelText: "",
+                                contentPadding: new EdgeInsets.symmetric(
+                                    vertical: 13.0, horizontal: 10.0),
+                                fillColor: MColors.primaryWhite,
+                                hasFloatingPlaceholder: false,
+                                filled: true,
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: Text(
+                        "Your password must have 6 or more characters and your password must contain at least one number.",
+                        style: TextStyle(color: MColors.primaryPurple),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "Phone number",
+                            style: TextStyle(color: MColors.textGrey),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: TextFormField(
+                            validator: (val) => !val.contains("47")
+                                ? 'Invalid Phone number'
+                                : null,
+                            onSaved: (val) => _phoneNumber = val,
+                            decoration: InputDecoration(
+                                labelText: "e.g 47-99875-2365",
+                                contentPadding: new EdgeInsets.symmetric(
+                                    vertical: 13.0, horizontal: 10.0),
+                                fillColor: MColors.primaryWhite,
+                                hasFloatingPlaceholder: false,
+                                filled: true,
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
