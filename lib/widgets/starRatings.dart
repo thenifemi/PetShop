21199330e65c85e8b8faa70e73ecaeae878ac672
Nifemi <thenifemi@gmail.com>
2020-04-07 18:48:08 -0300

    built popular block and shop by pet block| added mock data ALSO added star rating widget

---
+++ lib/widgets/starRatings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StarDisplay extends StatelessWidget {
+  final int value;
+
+  const StarDisplay({Key key, this.value = 0})
+      : assert(value != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < value ? Icons.star : Icons.star_border,
+        );
+      }),
+    );
+  }
+}
