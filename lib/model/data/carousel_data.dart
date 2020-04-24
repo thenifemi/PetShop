8f798aad0823d8305fbc42af75fa40cd8ba58b19
Nifemi <thenifemi@gmail.com>
2020-04-23 22:52:30 -0300

    Lots of changes 2

---
+++ lib/model/data/carousel_data.dart
@@ -0,0 +1,9 @@
+class CarouselData {
+  var carouselImages = [
+    "assets/images/productImages/vetdoc.jpg",
+    "assets/images/productImages/charities.png",
+    "assets/images/productImages/doggycamp.jpg",
+    "assets/images/productImages/pethotel.jpg",
+    "assets/images/productImages/training.jpg",
+  ];
+}
