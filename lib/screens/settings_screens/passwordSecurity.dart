9d60c36aaef5687556a4fa972564676734ac3e78
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-06-05 04:58:56 +0100

    implementation of location permission.

--- lib/screens/settings_screens/passwordSecurity.dart
+++ lib/screens/settings_screens/passwordSecurity.dart
@@ -3,6 +3,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mollet/screens/settings_screens/changePassword.dart';
 import 'package:mollet/utils/colors.dart';
+import 'package:mollet/utils/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SecurityScreen extends StatelessWidget {
   @override
@@ -30,52 +32,106 @@ class SecurityScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-        child: Column(children: <Widget>[
-          SizedBox(
-            height: 60,
-            width: double.infinity,
-            child: RawMaterialButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => ChangePasswordScreen(),
-                  ),
-                );
-              },
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: SvgPicture.asset(
-                      "assets/images/key.svg",
-                      height: 20,
-                      color: MColors.textGrey,
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      "Change password",
-                      style: GoogleFonts.montserrat(
-                        color: MColors.primaryPurple,
-                        fontSize: 15.0,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+            child: Column(children: <Widget>[
+              SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => ChangePasswordScreen(),
                       ),
-                    ),
+                    );
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: SvgPicture.asset(
+                          "assets/images/key.svg",
+                          height: 20,
+                          color: MColors.textGrey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Change password",
+                          style: GoogleFonts.montserrat(
+                            color: MColors.primaryPurple,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: MColors.textGrey,
+                        size: 16.0,
+                      ),
+                    ],
                   ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: MColors.textGrey,
-                    size: 16.0,
-                  ),
-                ],
+                ),
               ),
-            ),
+              Divider(
+                height: 1.0,
+              ),
+            ]),
           ),
-          Divider(
-            height: 1.0,
+
+          Container(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+            child: Column(children: <Widget>[
+              SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: RawMaterialButton(
+                  onPressed: () {
+
+                    PermissionUtil.isLocationServiceAndPermissionsActive().then((value) {
+                        if(value == false) {
+                          PermissionUtil.requestPermission(PermissionGroup.location);
+                        }
+                      }
+                    );
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: SvgPicture.asset(
+                          "assets/images/key.svg",
+                          height: 20,
+                          color: MColors.textGrey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Location feature",
+                          style: GoogleFonts.montserrat(
+                            color: MColors.primaryPurple,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: MColors.textGrey,
+                        size: 16.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(
+                height: 1.0,
+              ),
+            ]),
           ),
-        ]),
+        ],
       ),
     );
   }
