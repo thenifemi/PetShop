cbbe6003e10e7aec9c817c385fd576b7d5e3a5f1
Nifemi <thenifemi@gmail.com>
2020-06-06 13:20:26 -0300

    slight changes

--- lib/screens/home_screens/homeScreen_pages/productDetailsScreen.dart
+++ lib/screens/home_screens/homeScreen_pages/productDetailsScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:collection';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
