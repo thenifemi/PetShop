9529cc8d5ce8c45255cfd7361e10d24e776edd77
Nifemi <thenifemi@gmail.com>
2020-06-09 15:10:46 -0300

    file clean up

---
+++ lib/model/notifiers/brands_notifier.dart
@@ -0,0 +1,24 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:mollet/model/data/brands.dart';
+
+class BrandsNotifier with ChangeNotifier {
+  List<Brands> _brandsList = [];
+  Brands _brands;
+
+  UnmodifiableListView<Brands> get brandsList =>
+      UnmodifiableListView(_brandsList);
+
+  Brands get brands => _brands;
+
+  set brandsList(List<Brands> brandsList) {
+    _brandsList = brandsList;
+    notifyListeners();
+  }
+
+  set brands(Brands brands) {
+    _brands = brands;
+    notifyListeners();
+  }
+}
