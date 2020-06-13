b52ed7bbe095020ab56181a037adacf5f52c54c8
Nifemi <thenifemi@gmail.com>
2020-06-12 21:28:50 -0300

    code cleanup

--- lib/screens/tab_screens/cart1.dart
+++ lib/screens/tab_screens/cart1.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
