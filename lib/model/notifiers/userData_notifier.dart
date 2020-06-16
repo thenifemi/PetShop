d088268895fbe6286aa953120c91a34ace243e29
Nifemi <thenifemi@gmail.com>
2020-06-16 18:12:08 -0300

    added notifier for user data profile and address

---
+++ lib/model/notifiers/userData_notifier.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+
+//For Users profile
+class UserDataProfile with ChangeNotifier {
+  List<UserDataProfile> _userDataProfileList = [];
+  UserDataProfile _userDataProfile;
+
+  List<UserDataProfile> get userDataProfileList => _userDataProfileList;
+  UserDataProfile get userDataProfile => _userDataProfile;
+
+  set userDataProfileList(List<UserDataProfile> userDataProfileList) {
+    _userDataProfileList = userDataProfileList;
+    notifyListeners();
+  }
+
+  set userDataProfile(UserDataProfile userDataProfile) {
+    _userDataProfile = userDataProfile;
+    notifyListeners();
+  }
+}
+
+//For Users address
+class UserDataAddress with ChangeNotifier {
+  List<UserDataAddress> _userDataAddressList = [];
+  UserDataAddress _userDataAddress;
+
+  List<UserDataAddress> get userDataAddressList => _userDataAddressList;
+  UserDataAddress get userDataAddress => _userDataAddress;
+
+  set userDataAddressList(List<UserDataAddress> userDataAddressList) {
+    _userDataAddressList = userDataAddressList;
+    notifyListeners();
+  }
+
+  set userDataAddress(UserDataAddress userDataAddress) {
+    _userDataAddress = userDataAddress;
+    notifyListeners();
+  }
+}
