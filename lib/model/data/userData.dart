ff495aff1831fdd0e9674a5bd53bccece69f6327
Nifemi <thenifemi@gmail.com>
2020-06-16 17:52:43 -0300

    added user data for address and profile

---
+++ lib/model/data/userData.dart
@@ -0,0 +1,48 @@
+class UserDataProfile {
+  String name;
+  String phone;
+  String email;
+
+  UserDataProfile.fromMap(Map<String, dynamic> data) {
+    name = data["name"];
+    phone = data["phone"];
+    email = data["email"];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'phone': phone,
+      'email': email,
+    };
+  }
+}
+
+class UserDataAddress {
+  String fullLegalName;
+  String addressLine1;
+  String addressLine2;
+  String city;
+  String zipcode;
+  String state;
+
+  UserDataAddress.fromMap(Map<String, dynamic> data) {
+    fullLegalName = data["fullLegalName"];
+    addressLine1 = data["addressLine1"];
+    addressLine2 = data["addressLine2"];
+    city = data["city"];
+    zipcode = data["zipcode"];
+    state = data["state"];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'fullLegalName': fullLegalName,
+      'addressLine1': addressLine1,
+      'addressLine2': addressLine2,
+      'city': city,
+      'zipcode': zipcode,
+      'state': state,
+    };
+  }
+}
