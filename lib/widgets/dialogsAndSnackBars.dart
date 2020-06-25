462eab889327c3bfd50369e4c7337e42b2464aec
Nifemi <thenifemi@gmail.com>
2020-06-24 21:39:22 -0300

    updates

--- lib/widgets/dialogsAndSnackBars.dart
+++ lib/widgets/dialogsAndSnackBars.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+void showSimpleSnack(String value, IconData icon, Color iconColor) {
+  _scaffoldKey.currentState.showSnackBar(
+    SnackBar(
+      behavior: SnackBarBehavior.floating,
+      duration: Duration(milliseconds: 1400),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      content: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              value,
+              style: GoogleFonts.montserrat(),
+            ),
+          ),
+          Icon(
+            icon,
+            color: iconColor,
+          )
+        ],
+      ),
+    ),
+  );
+}
