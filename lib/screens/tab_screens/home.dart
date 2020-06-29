7fed6a62c0c0dff16e91bf15807c1975e880ef51
Nifemi <thenifemi@gmail.com>
2020-06-29 00:09:25 -0300

    code refactoring and app redesign

--- lib/screens/tab_screens/home.dart
+++ lib/screens/tab_screens/home.dart
@@ -35,29 +35,69 @@ class _HomeScreenState extends State<HomeScreen>
     "Small Pets",
   ];
 
-  @override
-  void initState() {
-    ProductsNotifier productsNotifier =
-        Provider.of<ProductsNotifier>(context, listen: false);
-    getProdProducts(productsNotifier);
-
+  void addToBagshowDialog(
+    cartProdID,
+    fil,
+  ) async {
     CartNotifier cartNotifier =
         Provider.of<CartNotifier>(context, listen: false);
-    getCart(cartNotifier);
 
-    // BrandsNotifier brandsNotifier =
-    //     Provider.of<BrandsNotifier>(context, listen: false);
-    // getBrands(brandsNotifier);
-
-    UserDataProfileNotifier profileNotifier =
-        Provider.of<UserDataProfileNotifier>(context, listen: false);
-    getProfile(profileNotifier);
-
-    _tabController = TabController(
-      length: _tabItems.length,
-      vsync: this,
-    );
-    super.initState();
+    await showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            content: Text(
+              "Sure you want to add to Bag?",
+              style: normalFont(MColors.textDark, null),
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text(
+                  "Cancel",
+                  style: normalFont(Colors.red, null),
+                ),
+                onPressed: () async {
+                  setState(() {
+                    getCart(cartNotifier);
+                  });
+                  Navigator.of(context).pop();
+                },
+              ),
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text(
+                  "Yes",
+                  style: normalFont(Colors.blue, null),
+                ),
+                onPressed: () {
+                  setState(() {
+                    getCart(cartNotifier);
+                  });
+                  if (cartProdID.contains(fil.productID)) {
+                    showSimpleSnack(
+                      "Product already in bag",
+                      Icons.error_outline,
+                      Colors.amber,
+                      _scaffoldKey,
+                    );
+                  } else {
+                    addProductToCart(fil);
+                    showSimpleSnack(
+                      "Product added to bag",
+                      Icons.check_circle_outline,
+                      Colors.green,
+                      _scaffoldKey,
+                    );
+                    setState(() {
+                      getCart(cartNotifier);
+                    });
+                  }
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 
   @override
@@ -147,71 +187,6 @@ class _HomeScreenState extends State<HomeScreen>
   }
 
   //Add to Bag dialog
-  void addToBagshowDialog(
-    cartProdID,
-    fil,
-  ) async {
-    CartNotifier cartNotifier =
-        Provider.of<CartNotifier>(context, listen: false);
-
-    await showCupertinoDialog(
-        context: context,
-        builder: (context) {
-          return CupertinoAlertDialog(
-            content: Text(
-              "Sure you want to add to Bag?",
-              style: normalFont(MColors.textDark, null),
-            ),
-            actions: <Widget>[
-              CupertinoDialogAction(
-                child: Text(
-                  "Cancel",
-                  style: normalFont(Colors.red, null),
-                ),
-                onPressed: () async {
-                  setState(() {
-                    getCart(cartNotifier);
-                  });
-                  Navigator.of(context).pop();
-                },
-              ),
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                child: Text(
-                  "Yes",
-                  style: normalFont(Colors.blue, null),
-                ),
-                onPressed: () {
-                  setState(() {
-                    getCart(cartNotifier);
-                  });
-                  if (cartProdID.contains(fil.productID)) {
-                    showSimpleSnack(
-                      "Product already in bag",
-                      Icons.error_outline,
-                      Colors.amber,
-                      _scaffoldKey,
-                    );
-                  } else {
-                    addProductToCart(fil);
-                    showSimpleSnack(
-                      "Product added to bag",
-                      Icons.check_circle_outline,
-                      Colors.green,
-                      _scaffoldKey,
-                    );
-                    setState(() {
-                      getCart(cartNotifier);
-                    });
-                  }
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        });
-  }
-
   Widget buildAllBody(prods, cartProdID) {
     Iterable<ProdProducts> all = prods.reversed;
 
@@ -229,6 +204,123 @@ class _HomeScreenState extends State<HomeScreen>
         crossAxisSpacing: 15.0,
         children: List<Widget>.generate(all.length, (i) {
           var cleanList = all.toList();
+          var fil = cleanList[i];
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailsProv(fil, prods),
+                ),
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: MColors.primaryWhite,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.03),
+                      offset: Offset(0, 10),
+                      blurRadius: 10,
+                      spreadRadius: 0),
+                ],
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Hero(
+                        child: FadeInImage.assetNetwork(
+                          image: fil.productImage,
+                          fit: BoxFit.fill,
+                          height: MediaQuery.of(context).size.height / 5.8,
+                          placeholder: "assets/images/placeholder.jpg",
+                          placeholderScale:
+                              MediaQuery.of(context).size.height / 2,
+                        ),
+                        tag: fil.productID,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      child: Text(
+                        fil.name,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: normalFont(MColors.textDark, 16.0),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            "\$${fil.price}",
+                            style: boldFont(MColors.primaryPurple, 20.0),
+                          ),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            addToBagshowDialog(
+                              cartProdID,
+                              fil,
+                            );
+                          },
+                          child: Container(
+                            width: 40.0,
+                            height: 40.0,
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: MColors.dashPurple,
+                              borderRadius: new BorderRadius.circular(8.0),
+                            ),
+                            child: SvgPicture.asset(
+                              "assets/images/icons/basket.svg",
+                              height: 22.0,
+                              color: MColors.textGrey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget buildCatBody(prods, cartProdID) {
+    Iterable<ProdProducts> cat = prods.where((e) => e.pet == "cat");
+
+    var size = MediaQuery.of(context).size;
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height) / 2.5;
+    final double itemWidth = size.width / 2;
+    return primaryContainer(
+      GridView.count(
+        physics: BouncingScrollPhysics(),
+        crossAxisCount: 2,
+        childAspectRatio: (itemWidth / itemHeight),
+        mainAxisSpacing: 15.0,
+        crossAxisSpacing: 15.0,
+        children: List<Widget>.generate(cat.length, (i) {
+          var cleanList = cat.toList();
           var fil = cleanList[i];
 
           return GestureDetector(
@@ -447,120 +539,28 @@ class _HomeScreenState extends State<HomeScreen>
     );
   }
 
-  Widget buildCatBody(prods, cartProdID) {
-    Iterable<ProdProducts> cat = prods.where((e) => e.pet == "cat");
+  @override
+  void initState() {
+    ProductsNotifier productsNotifier =
+        Provider.of<ProductsNotifier>(context, listen: false);
+    getProdProducts(productsNotifier);
 
-    var size = MediaQuery.of(context).size;
-    /*24 is for notification bar on Android*/
-    final double itemHeight = (size.height) / 2.5;
-    final double itemWidth = size.width / 2;
-    return primaryContainer(
-      GridView.count(
-        physics: BouncingScrollPhysics(),
-        crossAxisCount: 2,
-        childAspectRatio: (itemWidth / itemHeight),
-        mainAxisSpacing: 15.0,
-        crossAxisSpacing: 15.0,
-        children: List<Widget>.generate(cat.length, (i) {
-          var cleanList = cat.toList();
-          var fil = cleanList[i];
+    CartNotifier cartNotifier =
+        Provider.of<CartNotifier>(context, listen: false);
+    getCart(cartNotifier);
 
-          return GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ProductDetailsProv(fil, prods),
-                ),
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: MColors.primaryWhite,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.03),
-                      offset: Offset(0, 10),
-                      blurRadius: 10,
-                      spreadRadius: 0),
-                ],
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Hero(
-                        child: FadeInImage.assetNetwork(
-                          image: fil.productImage,
-                          fit: BoxFit.fill,
-                          height: MediaQuery.of(context).size.height / 5.8,
-                          placeholder: "assets/images/placeholder.jpg",
-                          placeholderScale:
-                              MediaQuery.of(context).size.height / 2,
-                        ),
-                        tag: fil.productID,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Container(
-                      child: Text(
-                        fil.name,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: normalFont(MColors.textDark, 16.0),
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          child: Text(
-                            "\$${fil.price}",
-                            style: boldFont(MColors.primaryPurple, 20.0),
-                          ),
-                        ),
-                        Spacer(),
-                        GestureDetector(
-                          onTap: () {
-                            addToBagshowDialog(
-                              cartProdID,
-                              fil,
-                            );
-                          },
-                          child: Container(
-                            width: 40.0,
-                            height: 40.0,
-                            padding: const EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              color: MColors.dashPurple,
-                              borderRadius: new BorderRadius.circular(8.0),
-                            ),
-                            child: SvgPicture.asset(
-                              "assets/images/icons/basket.svg",
-                              height: 22.0,
-                              color: MColors.textGrey,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }),
-      ),
+    // BrandsNotifier brandsNotifier =
+    //     Provider.of<BrandsNotifier>(context, listen: false);
+    // getBrands(brandsNotifier);
+
+    UserDataProfileNotifier profileNotifier =
+        Provider.of<UserDataProfileNotifier>(context, listen: false);
+    getProfile(profileNotifier);
+
+    _tabController = TabController(
+      length: _tabItems.length,
+      vsync: this,
     );
+    super.initState();
   }
 }
