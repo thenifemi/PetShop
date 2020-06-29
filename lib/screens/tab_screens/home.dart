b1ce048bab9dac6343cae191e91a418a2ddf8dfe
Nifemi <thenifemi@gmail.com>
2020-06-28 21:13:06 -0300

    code refactoring and app redesign

--- lib/screens/tab_screens/home.dart
+++ lib/screens/tab_screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mollet/model/data/Products.dart';
 import 'package:mollet/model/notifiers/cart_notifier.dart';
 import 'package:mollet/model/notifiers/products_notifier.dart';
