b37672877822477936f39f6629f04008095325a2
Nifemi <thenifemi@gmail.com>
2020-07-12 20:18:41 -0300

    implementing autocomplete search

--- lib/screens/tab_screens/checkout_screens/enterAddress.dart
+++ lib/screens/tab_screens/checkout_screens/enterAddress.dart
@@ -12,7 +12,7 @@ class _EnterAddressState extends State<EnterAddress> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MColors.primaryWhite,
+      backgroundColor: MColors.primaryWhiteSmoke,
       appBar: primaryAppBar(
         IconButton(
           icon: Icon(
@@ -58,6 +58,47 @@ class _EnterAddressState extends State<EnterAddress> {
                       ),
                     ),
                   ),
+                  SizedBox(height: 10.0),
+                  Center(
+                    child: Text(
+                      "Or",
+                      style: boldFont(MColors.textGrey, 14.0),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: MColors.primaryWhite,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              child: SvgPicture.asset(
+                                "assets/images/icons/Discovery.svg",
+                                color: MColors.primaryPurple,
+                              ),
+                            ),
+                            SizedBox(width: 5.0),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  "Use current location",
+                                  style: boldFont(MColors.textDark, 14.0),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
