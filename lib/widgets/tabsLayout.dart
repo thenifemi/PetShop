9772422a34f6965a2ed20d083871bbcd5f03a6ff
Nifemi <thenifemi@gmail.com>
2020-07-22 23:50:05 -0300

    UI changes

--- lib/widgets/tabsLayout.dart
+++ lib/widgets/tabsLayout.dart
@@ -93,11 +93,11 @@ class _TabsLayoutState extends State<TabsLayout> {
                         .replaceAll("\(", ""),
                     style: boldFont(MColors.textGrey, 20.0),
                   )
-                : Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          // width: MediaQuery.of(context).size.width / 1.22,
+                : Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
                           child: primaryTextField(
                             null,
                             null,
@@ -118,53 +118,55 @@ class _TabsLayoutState extends State<TabsLayout> {
                             0.0,
                           ),
                         ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(5.0),
-                        width: 40.0,
-                        child: Stack(
-                          children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.only(top: 5.0),
-                              child: SvgPicture.asset(
-                                "assets/images/icons/Bag.svg",
-                                height: 25,
-                                color: MColors.textGrey,
+                        Container(
+                          padding: const EdgeInsets.all(5.0),
+                          // width: 40.0,
+                          child: Stack(
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.only(top: 5.0),
+                                child: SvgPicture.asset(
+                                  "assets/images/icons/Bag.svg",
+                                  height: 25,
+                                  color: MColors.textGrey,
+                                ),
                               ),
-                            ),
-                            cartList.isNotEmpty
-                                ? Positioned(
-                                    right: 0,
-                                    child: new Container(
-                                      padding: EdgeInsets.all(1),
-                                      decoration: new BoxDecoration(
-                                        color: Colors.redAccent,
-                                        borderRadius: BorderRadius.circular(6),
+                              cartList.isNotEmpty
+                                  ? Positioned(
+                                      right: 0,
+                                      child: new Container(
+                                        padding: EdgeInsets.all(1),
+                                        decoration: new BoxDecoration(
+                                          color: Colors.redAccent,
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                        ),
+                                        constraints: BoxConstraints(
+                                          minWidth: 7,
+                                          minHeight: 7,
+                                        ),
                                       ),
-                                      constraints: BoxConstraints(
-                                        minWidth: 7,
-                                        minHeight: 7,
+                                    )
+                                  : Positioned(
+                                      right: 0,
+                                      child: new Container(
+                                        padding: EdgeInsets.all(1),
+                                        decoration: new BoxDecoration(
+                                          color: Colors.transparent,
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                        ),
+                                        constraints: BoxConstraints(
+                                          minWidth: 7,
+                                          minHeight: 7,
+                                        ),
                                       ),
                                     ),
-                                  )
-                                : Positioned(
-                                    right: 0,
-                                    child: new Container(
-                                      padding: EdgeInsets.all(1),
-                                      decoration: new BoxDecoration(
-                                        color: Colors.transparent,
-                                        borderRadius: BorderRadius.circular(6),
-                                      ),
-                                      constraints: BoxConstraints(
-                                        minWidth: 7,
-                                        minHeight: 7,
-                                      ),
-                                    ),
-                                  ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
           ],
         ),
