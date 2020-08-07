46bcf00497f1f84588476e7d47af030dbedb3c4f
Nifemi <thenifemi@gmail.com>
2020-08-07 00:29:37 -0300

    .

--- lib/utils/permission_handler.dart
+++ lib/utils/permission_handler.dart
@@ -2,7 +2,6 @@ import 'package:geolocation/geolocation.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionUtil {
-
   // Allows request any permission in the Permission Group with this method
   static Future<bool> requestPermission(PermissionGroup permission) async {
     final PermissionHandler _permissionHandler = PermissionHandler();
@@ -17,7 +16,8 @@ class PermissionUtil {
   // Allows checks on any permission in the Permission Group with this method
   static Future<bool> hasPermission(PermissionGroup permission) async {
     final PermissionHandler _permissionHandler = PermissionHandler();
-    var permissionStatus = await _permissionHandler.checkPermissionStatus(permission);
+    var permissionStatus =
+        await _permissionHandler.checkPermissionStatus(permission);
     return permissionStatus == PermissionStatus.granted;
   }
 
@@ -25,21 +25,25 @@ class PermissionUtil {
 
   static Future<bool> isGpsServiceActive() async {
     final GeolocationResult gps = await Geolocation.isLocationOperational();
-    if(!gps.isSuccessful) {
+    if (!gps.isSuccessful) {
       await Geolocation.enableLocationServices()
           .then((GeolocationResult onValue) async {
-        if (onValue.isSuccessful) return true;
-        else return false;
+        if (onValue.isSuccessful)
+          return true;
+        else
+          return false;
       });
     }
     return true;
   }
 
   static Future<bool> isLocationServiceAndPermissionsActive() async {
-    final GeolocationResult gpsServiceActive = await Geolocation.isLocationOperational();
-    final gpsPermissionGranted = await PermissionUtil.hasPermission(PermissionGroup.locationWhenInUse);
+    final GeolocationResult gpsServiceActive =
+        await Geolocation.isLocationOperational();
+    final gpsPermissionGranted =
+        await PermissionUtil.hasPermission(PermissionGroup.locationWhenInUse);
 
-    if(gpsServiceActive.isSuccessful == false || gpsPermissionGranted == false)
+    if (gpsServiceActive.isSuccessful == false || gpsPermissionGranted == false)
       return false;
 
     return true;
