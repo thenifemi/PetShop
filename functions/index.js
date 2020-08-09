8667b4e4a67aed53b78d9838fd880f9b200f3bd4
Nifemi <thenifemi@gmail.com>
2020-08-09 19:38:17 -0300

    .

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,30 @@ admin.initializeApp(functions.config().firebase);
 
 var msgData;
 
-exports.orderTrigger = functions.firestore.document(
-  "userOrder/{userOrderId}/orders/{ordersId}"
-).onCreate((snapshot, context) => msgData = snapshot.data);
+var uEmail = await AuthService().getCurrentEmail();
+
+exports.orderTrigger = functions.firestore
+  .document("userOrder/{userOrderId}/orders/{ordersId}")
+  .onCreate((snapshot, context) => {
+    msgData = snapshot.data;
+
+    admin
+      .firestore()
+      .collection("userData")
+      .document(uEmail)
+      .collection("tokens")
+      .get()
+      .then((snapshots) => {
+        var token;
+
+        snapshots.empty
+          ? console.log("No Devices")
+          : (token = snapshots.data().token);
+
+        var payload = { 
+            "notifications" :{
+                "title" : "order" + msgData.orderID
+            }
+        }
+      });
+  });
