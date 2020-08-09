1d5a9b73cc4699e361df757d6a61d7eab459a721
Nifemi <thenifemi@gmail.com>
2020-08-09 19:22:59 -0300

    .

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const functions = require("firebase-admin");
+const { admin } = require("firebase-admin/lib/database");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+var msgData;
+
+exports.orderTrigger = functions.firestore.document(
+  "userOrder/{userOrderId}/orders/{ordersId}"
+).onCreate((snapshot, context) => msgData = snapshot.data);
