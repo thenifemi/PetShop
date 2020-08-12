63b15dcf513c9b5ff0efce0701e29298c8af149d
Nifemi <thenifemi@gmail.com>
2020-08-12 16:02:56 -0300

    .

--- lib/model/data/notificationMessage.dart
+++ lib/model/data/notificationMessage.dart
@@ -7,5 +7,21 @@ class NotificationMessage {
   String notificationTitle;
   String notificationBody;
 
-  NotificationMessage.fromMap(Map<String, dynamic> data) {}
+  NotificationMessage.fromMap(Map<String, dynamic> data) {
+    senderAvatar = data["senderAvatar"];
+    senderName = data["senderName"];
+    sentTime = data["sentTime"];
+    notificationTitle = data["notificationTitle"];
+    notificationBody = data["notificationBody"];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'senderAvatar': senderAvatar,
+      'senderName': senderName,
+      'sentTime': sentTime,
+      'notificationTitle': notificationTitle,
+      'notificationBody': notificationBody,
+    };
+  }
 }
