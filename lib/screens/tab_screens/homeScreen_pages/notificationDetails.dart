c9b425127bc16cc04a6734eb0674e1edbcd09280
Nifemi <thenifemi@gmail.com>
2020-08-12 15:47:12 -0300

    .

---
+++ lib/screens/tab_screens/homeScreen_pages/notificationDetails.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:mollet/widgets/allWidgets.dart';
+
+class NotificationsDetails extends StatelessWidget {
+  const NotificationsDetails({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return primaryContainer(
+      Container(),
+    );
+  }
+}
