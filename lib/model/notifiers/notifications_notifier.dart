7988034d9e1af9ce7dad1f5ee8dfce4731540442
Nifemi <thenifemi@gmail.com>
2020-08-12 16:09:08 -0300

    notification service

---
+++ lib/model/notifiers/notifications_notifier.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:mollet/model/data/notificationMessage.dart';
+
+class NotificationsNotifier with ChangeNotifier {
+  List<NotificationMessage> _notificationMessageList = [];
+
+  List<NotificationMessage> get notificationMessageList =>
+      _notificationMessageList;
+
+  set notificationMessageList(
+      List<NotificationMessage> notificatioMessageList) {
+    _notificationMessageList = notificatioMessageList;
+    notifyListeners();
+  }
+}
