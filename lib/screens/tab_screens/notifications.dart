76579106fa2fccfa622270275305b1a82dab80c5
Nifemi <thenifemi@gmail.com>
2020-08-11 21:27:02 -0300

    .

--- lib/screens/tab_screens/notifications.dart
+++ lib/screens/tab_screens/notifications.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mollet/utils/colors.dart';
 import 'package:mollet/widgets/allWidgets.dart';
 
 // import 'dart:async';
@@ -19,6 +20,36 @@ class _InboxScreenState extends State<InboxScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return primaryContainer(
+      ListView.builder(
+          physics: BouncingScrollPhysics(),
+          itemCount: 5,
+          itemBuilder: (context, i) {
+            return Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: Image.asset(
+                          "assets/images/footprint.png",
+                          height: 90,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: MColors.dashPurple,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+
+  Widget noNotifications() {
     return emptyScreen(
       "assets/images/noInbox.svg",
       "No Notifications",
