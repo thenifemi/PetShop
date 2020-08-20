f891a1b1fcb91d878b89e56fdfc04fa58eb4e7b8
Nifemi <thenifemi@gmail.com>
2020-08-19 22:19:06 -0300

    .

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -5,19 +5,4 @@ class Strings {
   static const String onBoardTitle_sub1 =
       "Caring for your pets has never been so easy! With Pet Shop, you can get pet supplies, hire professional pet services and book Vet appointments.";
   static const String onBoardTitle2 = "the World of Pets";
-
-  static const String fingerprintTitle = "Fingerprint";
-  static const String fingerprintTitle_sub = "Please scan your finger";
-  static const String login_to_account = "Sign in to your account";
-  static const String do_not_have_account = " Do not have an account?";
-  static const String forgot_password = "Forgot password?";
-  static const String fingerprint_authTitle = "Fingerprint Authentication";
-  static const String fingerprint_authTitle_sub =
-      "Please scan your finger to log in";
-  static const String link_account = "Link account";
-  static const String pick_card = "International card";
-  static const String link_card = "Link your card";
-  static const String link_success = "Card linked successfully!";
-  static const String link_success_sub = "Now lets get your finances in check";
-  static const String getStarted = "Get started";
 }
