97f2b8b23cea109fac8919d3968282278449733c
Nifemi <thenifemi@gmail.com>
2020-08-19 22:45:33 -0300

    .

--- lib/screens/getstarted_screens/intro_screen.dart
+++ lib/screens/getstarted_screens/intro_screen.dart
@@ -7,81 +7,122 @@ import 'package:mollet/utils/colors.dart';
 import 'package:mollet/utils/strings.dart';
 import 'package:mollet/widgets/allWidgets.dart';
 
-class IntroScreen extends StatelessWidget {
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: primaryContainer(
-        Center(
+    return WillPopScope(
+      onWillPop: () {
+        return Future.value(true);
+      },
+      child: Scaffold(
+        body: primaryContainer(
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SvgPicture.asset(
+                    "assets/images/pets.svg",
+                    height: 200,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Container(
+                  child: Text(
+                    "Welcome to Pet Shop",
+                    style: boldFont(MColors.textDark, 30.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  child: Text(
+                    Strings.onBoardTitle_sub1,
+                    style: normalFont(MColors.textGrey, 18.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          height: 150.0,
+          color: MColors.primaryWhite,
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: SvgPicture.asset(
-                  "assets/images/pets.svg",
-                  height: 200,
+              primaryButtonPurple(
+                Text(
+                  "Sign in",
+                  style: boldFont(MColors.primaryWhite, 16.0),
                 ),
+                () {
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (_) => LoginScreen(),
+                    ),
+                  );
+                },
               ),
-              SizedBox(height: 30.0),
-              Container(
-                child: Text(
-                  "Welcome to Pet Shop",
-                  style: boldFont(MColors.textDark, 30.0),
-                  textAlign: TextAlign.center,
-                ),
+              SizedBox(
+                height: 10.0,
               ),
-              SizedBox(height: 10.0),
-              Container(
-                child: Text(
-                  Strings.onBoardTitle_sub1,
-                  style: normalFont(MColors.textGrey, 18.0),
-                  textAlign: TextAlign.center,
+              primaryButtonWhiteSmoke(
+                Text(
+                  "Create an account",
+                  style: boldFont(MColors.primaryPurple, 16.0),
                 ),
+                () {
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (_) => RegistrationScreen(),
+                    ),
+                  );
+                },
               ),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-        height: 150.0,
-        color: MColors.primaryWhite,
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            primaryButtonPurple(
-              Text(
-                "Sign in",
-                style: boldFont(MColors.primaryWhite, 16.0),
-              ),
-              () {
-                Navigator.of(context).pushReplacement(
-                  CupertinoPageRoute(
-                    builder: (_) => LoginScreen(),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            primaryButtonWhiteSmoke(
-              Text(
-                "Create an account",
-                style: boldFont(MColors.primaryPurple, 16.0),
-              ),
-              () {
-                Navigator.of(context).pushReplacement(
-                  CupertinoPageRoute(
-                    builder: (_) => RegistrationScreen(),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
     );
+  }
+
+  void _showCloseAppDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(
+              "Are you sure you want to leave?",
+              style: normalFont(MColors.textGrey, 14.0),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "Cancel",
+                  style: normalFont(MColors.textGrey, 14.0),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Leave",
+                  style: normalFont(Colors.redAccent, 14.0),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
