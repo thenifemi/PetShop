cb6ae124dbfd413ecffc066abfc2b689740351ff
Nifemi <thenifemi@gmail.com>
2020-08-19 22:19:30 -0300

    .

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,7 +1,6 @@
 class Strings {
   //GET STARTED STRINGS
   static String appName = "Pet Shop";
-  static const String onBoardTitle1 = "Welcome to";
   static const String onBoardTitle_sub1 =
       "Caring for your pets has never been so easy! With Pet Shop, you can get pet supplies, hire professional pet services and book Vet appointments.";
 }
