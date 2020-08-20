304009f0c78c055a7cbf41e031b632e6f9e631b1
Nifemi <thenifemi@gmail.com>
2020-08-19 23:18:46 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:mollet/model/services/auth_service.dart';
 import 'package:mollet/screens/getstarted_screens/intro_screen.dart';
 import 'package:mollet/screens/getstarted_screens/splash_screen.dart';
 import 'package:mollet/utils/colors.dart';
+import 'package:mollet/widgets/allWidgets.dart';
 import 'package:mollet/widgets/provider.dart';
 import 'package:mollet/widgets/tabsLayout.dart';
 import 'package:provider/provider.dart';
@@ -53,49 +54,88 @@ class _HomeControllerState extends State<HomeController> {
   Widget build(BuildContext context) {
     final AuthService auth = MyProvider.of(context).auth;
 
-    return StreamBuilder(
-        stream: auth.onAuthStateChanged,
-        builder: (context, AsyncSnapshot<String> snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            final bool signedIn = snapshot.hasData;
-            return MaterialApp(
-              title: "Pet Shop",
-              theme: ThemeData(
-                accentColor: MColors.primaryPurple,
-                primaryColor: MColors.primaryPurple,
+    return WillPopScope(
+      onWillPop: () {
+        _showCloseAppDialog();
+        return Future.value(true);
+      },
+      child: StreamBuilder(
+          stream: auth.onAuthStateChanged,
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              final bool signedIn = snapshot.hasData;
+              return MaterialApp(
+                title: "Pet Shop",
+                theme: ThemeData(
+                  accentColor: MColors.primaryPurple,
+                  primaryColor: MColors.primaryPurple,
+                ),
+                debugShowCheckedModeBanner: false,
+                home: signedIn
+                    ? MultiProvider(
+                        providers: [
+                          ChangeNotifierProvider(
+                            create: (context) => ProductsNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => BrandsNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => CartNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => UserDataProfileNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => UserDataAddressNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => OrderListNotifier(),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (context) => NotificationsNotifier(),
+                          ),
+                        ],
+                        child: TabsLayout(),
+                      )
+                    : IntroScreen(),
+              );
+            }
+            return SplashScreen();
+          }),
+    );
+  }
+
+  void _showCloseAppDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(
+              "Are you sure you want to leave?",
+              style: normalFont(MColors.textGrey, 14.0),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "Cancel",
+                  style: normalFont(MColors.textGrey, 14.0),
+                ),
               ),
-              debugShowCheckedModeBanner: false,
-              home: signedIn
-                  ? MultiProvider(
-                      providers: [
-                        ChangeNotifierProvider(
-                          create: (context) => ProductsNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => BrandsNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => CartNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => UserDataProfileNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => UserDataAddressNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => OrderListNotifier(),
-                        ),
-                        ChangeNotifierProvider(
-                          create: (context) => NotificationsNotifier(),
-                        ),
-                      ],
-                      child: TabsLayout(),
-                    )
-                  : IntroScreen(),
-            );
-          }
-          return SplashScreen();
+              FlatButton(
+                onPressed: () {
+                  SystemNavigator.pop();
+                },
+                child: Text(
+                  "Leave",
+                  style: normalFont(Colors.redAccent, 14.0),
+                ),
+              ),
+            ],
+          );
         });
   }
 }
