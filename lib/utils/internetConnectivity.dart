17aa1e2a73cf1173557609dcbe08b0917b2a8f64
Nifemi <thenifemi@gmail.com>
2020-08-24 23:53:03 -0300

    check for internet connectivity

---
+++ lib/utils/internetConnectivity.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+Future checkInternetConnectivity() async {
+  bool isConnected;
+  try {
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      isConnected = true;
+    }
+  } on SocketException catch (_) {
+    isConnected = false;
+  }
+  return isConnected;
+}
