d3f444b6990896588bb5c4eaad0a94e829f1f022
Nifemi <thenifemi@gmail.com>
2020-08-30 23:31:23 -0300

    ..

--- lib/screens/tab_screens/search_screens/search_screen.dart
+++ lib/screens/tab_screens/search_screens/search_screen.dart
@@ -1,15 +1,189 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mollet/model/data/Products.dart';
+import 'package:mollet/model/notifiers/brands_notifier.dart';
+import 'package:mollet/model/notifiers/cart_notifier.dart';
+import 'package:mollet/model/notifiers/products_notifier.dart';
+import 'package:mollet/model/services/Product_service.dart';
+import 'package:mollet/screens/tab_screens/search_screens/search_tabs.dart';
+import 'package:mollet/utils/colors.dart';
+import 'package:mollet/utils/internetConnectivity.dart';
+import 'package:mollet/widgets/allWidgets.dart';
+import 'package:provider/provider.dart';
 
-class SeacrhScreen extends StatefulWidget {
-  SeacrhScreen({Key key}) : super(key: key);
-
-  @override
-  _SeacrhScreenState createState() => _SeacrhScreenState();
-}
-
-class _SeacrhScreenState extends State<SeacrhScreen> {
+class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProductsNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => BrandsNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CartNotifier(),
+        ),
+      ],
+      child: SearchScreen(),
+    );
+  }
+}
+
+class SearchScreen extends StatefulWidget {
+  SearchScreen({Key key}) : super(key: key);
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    checkInternetConnectivity().then((value) => {
+          value == true
+              ? () {
+                  // BrandsNotifier brandsNotifier =
+                  //     Provider.of<BrandsNotifier>(context, listen: false);
+                  // getBrands(brandsNotifier);
+
+                  ProductsNotifier productsNotifier =
+                      Provider.of<ProductsNotifier>(context, listen: false);
+                  getProdProducts(productsNotifier);
+
+                  CartNotifier cartNotifier =
+                      Provider.of<CartNotifier>(context, listen: false);
+                  getCart(cartNotifier);
+                }()
+              : showNoInternetSnack(_scaffoldKey)
+        });
+
+    _tabController = TabController(
+      length: _tabItems.length,
+      vsync: this,
+    );
+
+    super.initState();
+  }
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final PageStorageBucket searchBucket = PageStorageBucket();
+
+  TabController _tabController;
+  final _tabItems = [
+    "All",
+    "Dogs",
+    "Cats",
+    "Fish",
+    "Birds",
+    "Reptiles",
+    "Others",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    ProductsNotifier productsNotifier = Provider.of<ProductsNotifier>(context);
+    var prods = productsNotifier.productsList;
+
+    CartNotifier cartNotifier = Provider.of<CartNotifier>(context);
+    var cartList = cartNotifier.cartList;
+    var cartProdID = cartList.map((e) => e.productID);
+
+    //Tab Items
+    final _tabBody = [
+      buildAllBody(prods, cartProdID),
+      buildDogBody(prods, cartProdID),
+      buildCatBody(prods, cartProdID),
+      buildDogBody(prods, cartProdID),
+      buildCatBody(prods, cartProdID),
+      buildDogBody(prods, cartProdID),
+      buildCatBody(prods, cartProdID),
+    ];
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: primaryAppBar(
+        null,
+        TabBar(
+          unselectedLabelColor: MColors.textGrey,
+          unselectedLabelStyle: normalFont(MColors.textGrey, 16.0),
+          labelColor: MColors.primaryPurple,
+          labelStyle: boldFont(MColors.primaryPurple, 20.0),
+          indicatorWeight: 0.01,
+          isScrollable: true,
+          tabs: _tabItems.map((e) {
+            return Tab(
+              child: Text(
+                e,
+              ),
+            );
+          }).toList(),
+          controller: _tabController,
+        ),
+        MColors.primaryWhiteSmoke,
+        null,
+        false,
+        null,
+      ),
+      body: PageStorage(
+        bucket: searchBucket,
+        child: Container(
+          color: MColors.primaryWhiteSmoke,
+          child: prods.isEmpty
+              ? progressIndicator(MColors.primaryPurple)
+              : TabBarView(
+                  physics: BouncingScrollPhysics(),
+                  children: _tabBody,
+                  controller: _tabController,
+                ),
+        ),
+      ),
+    );
+  }
+
+  //Build Tabs
+  Widget buildAllBody(prods, cartProdID) {
+    Iterable<ProdProducts> all = prods.reversed;
+    CartNotifier cartNotifier =
+        Provider.of<CartNotifier>(context, listen: false);
+    ProductsNotifier productsNotifier =
+        Provider.of<ProductsNotifier>(context, listen: false);
+
+    return SearchTabWidget(
+      prods: all,
+      cartNotifier: cartNotifier,
+      productsNotifier: productsNotifier,
+      cartProdID: cartProdID,
+    );
+  }
+
+  Widget buildDogBody(prods, cartProdID) {
+    Iterable<ProdProducts> dog = prods.where((e) => e.pet == "dog");
+    CartNotifier cartNotifier =
+        Provider.of<CartNotifier>(context, listen: false);
+    ProductsNotifier productsNotifier = Provider.of<ProductsNotifier>(context);
+
+    return SearchTabWidget(
+      prods: dog,
+      cartNotifier: cartNotifier,
+      productsNotifier: productsNotifier,
+      cartProdID: cartProdID,
+    );
+  }
+
+  Widget buildCatBody(prods, cartProdID) {
+    Iterable<ProdProducts> cat = prods.where((e) => e.pet == "cat");
+    CartNotifier cartNotifier =
+        Provider.of<CartNotifier>(context, listen: false);
+    ProductsNotifier productsNotifier = Provider.of<ProductsNotifier>(context);
+
+    return SearchTabWidget(
+      prods: cat,
+      cartNotifier: cartNotifier,
+      productsNotifier: productsNotifier,
+      cartProdID: cartProdID,
+    );
   }
 }
