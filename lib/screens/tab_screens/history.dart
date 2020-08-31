667944bb04d764f96d6f266d92742251f2916bf2
Nifemi <thenifemi@gmail.com>
2020-08-30 23:14:15 -0300

    ..

--- lib/screens/tab_screens/history.dart
+++ lib/screens/tab_screens/history.dart
@@ -51,32 +51,29 @@ class _HistoryScreenState extends State<HistoryScreen>
     var orderList = orderListNotifier.orderListList;
 
     return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () => getOrders(orderListNotifier),
-        child: FutureBuilder(
-          future: ordersFuture,
-          builder: (c, s) {
-            switch (s.connectionState) {
-              case ConnectionState.active:
-                return progressIndicator(MColors.primaryPurple);
-                break;
-              case ConnectionState.done:
-                return orderList.isEmpty
-                    ? emptyScreen(
-                        "assets/images/noHistory.svg",
-                        "No Orders",
-                        "Your past orders, transactions and hires will show up here.",
-                      )
-                    : ordersScreen(orderList);
-                break;
-              case ConnectionState.waiting:
-                return progressIndicator(MColors.primaryPurple);
-                break;
-              default:
-                return progressIndicator(MColors.primaryPurple);
-            }
-          },
-        ),
+      body: FutureBuilder(
+        future: ordersFuture,
+        builder: (c, s) {
+          switch (s.connectionState) {
+            case ConnectionState.active:
+              return progressIndicator(MColors.primaryPurple);
+              break;
+            case ConnectionState.done:
+              return orderList.isEmpty
+                  ? emptyScreen(
+                      "assets/images/noHistory.svg",
+                      "No Orders",
+                      "Your past orders, transactions and hires will show up here.",
+                    )
+                  : ordersScreen(orderList);
+              break;
+            case ConnectionState.waiting:
+              return progressIndicator(MColors.primaryPurple);
+              break;
+            default:
+              return progressIndicator(MColors.primaryPurple);
+          }
+        },
       ),
     );
   }
@@ -122,141 +119,146 @@ class _HistoryScreenState extends State<HistoryScreen>
   }
 
   Widget currentOrder(orderList) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      child: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: orderList.length,
-        itemBuilder: (context, i) {
-          OrderListNotifier orderListNotifier =
-              Provider.of<OrderListNotifier>(context);
-          var _orderList = orderListNotifier.orderListList;
+    OrderListNotifier orderListNotifier =
+        Provider.of<OrderListNotifier>(context, listen: false);
+    return RefreshIndicator(
+      onRefresh: () => getOrders(orderListNotifier),
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: orderList.length,
+          itemBuilder: (context, i) {
+            OrderListNotifier orderListNotifier =
+                Provider.of<OrderListNotifier>(context);
+            var _orderList = orderListNotifier.orderListList;
 
-          var orderListItem = _orderList[i];
-          var orderID = orderListItem.orderID.substring(
-            orderListItem.orderID.length - 11,
-          );
+            var orderListItem = _orderList[i];
+            var orderID = orderListItem.orderID.substring(
+              orderListItem.orderID.length - 11,
+            );
 
-          var order = _orderList[i].order.toList();
-          var orderTotalPriceList = order.map((e) => e['totalPrice']);
-          var orderTotalPrice = orderTotalPriceList
-              .reduce((sum, element) => sum + element)
-              .toStringAsFixed(2);
-          var orderDayTime = orderListItem.orderDate.toDate().day.toString() +
-              "-" +
-              orderListItem.orderDate.toDate().month.toString() +
-              "-" +
-              orderListItem.orderDate.toDate().year.toString();
+            var order = _orderList[i].order.toList();
+            var orderTotalPriceList = order.map((e) => e['totalPrice']);
+            var orderTotalPrice = orderTotalPriceList
+                .reduce((sum, element) => sum + element)
+                .toStringAsFixed(2);
+            var orderDayTime = orderListItem.orderDate.toDate().day.toString() +
+                "-" +
+                orderListItem.orderDate.toDate().month.toString() +
+                "-" +
+                orderListItem.orderDate.toDate().year.toString();
 
-          return Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.only(bottom: 10.0),
-            decoration: BoxDecoration(
-              color: MColors.primaryWhite,
-              borderRadius: BorderRadius.all(
-                Radius.circular(10.0),
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.only(bottom: 10.0),
+              decoration: BoxDecoration(
+                color: MColors.primaryWhite,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
               ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        child: Row(
-                          children: <Widget>[
-                            Text(
-                              "Order No. ",
-                              style: normalFont(MColors.textGrey, 14.0),
-                            ),
-                            Text(
-                              orderID,
-                              style: boldFont(MColors.textGrey, 14.0),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Text(
-                        orderDayTime,
-                        style: normalFont(MColors.textGrey, 14.0),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  height: 70.0,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    physics: BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: order.length,
-                    itemBuilder: (context, i) {
-                      return Container(
-                        margin: EdgeInsets.symmetric(horizontal: 2),
-                        height: 50.0,
-                        width: 40.0,
-                        child: FadeInImage.assetNetwork(
-                          image: order[i]['productImage'],
-                          fit: BoxFit.fill,
-                          placeholder: "assets/images/placeholder.jpg",
-                          placeholderScale:
-                              MediaQuery.of(context).size.height / 2,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  child: Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
                     children: <Widget>[
-                      Text(
-                        order.length.toString() + " Items",
-                        style: normalFont(MColors.textGrey, 14.0),
-                      ),
-                      Spacer(),
-                      Text(
-                        "\$" + orderTotalPrice.toString(),
-                        style: boldFont(MColors.textGrey, 14.0),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        width: 60.0,
-                        height: 25.0,
-                        child: RawMaterialButton(
-                          onPressed: () {
-                            _showModalSheet(orderListItem);
-                          },
-                          child: Text(
-                            "Details",
-                            style: boldFont(MColors.primaryPurple, 14.0),
+                      Expanded(
+                        child: Container(
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                "Order No. ",
+                                style: normalFont(MColors.textGrey, 14.0),
+                              ),
+                              Text(
+                                orderID,
+                                style: boldFont(MColors.textGrey, 14.0),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                      Spacer(),
-                      Text(
-                        orderListItem.orderStatus,
-                        style: boldFont(Colors.green, 14.0),
+                      Container(
+                        child: Text(
+                          orderDayTime,
+                          style: normalFont(MColors.textGrey, 14.0),
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          );
-        },
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    height: 70.0,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: order.length,
+                      itemBuilder: (context, i) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(horizontal: 2),
+                          height: 50.0,
+                          width: 40.0,
+                          child: FadeInImage.assetNetwork(
+                            image: order[i]['productImage'],
+                            fit: BoxFit.fill,
+                            placeholder: "assets/images/placeholder.jpg",
+                            placeholderScale:
+                                MediaQuery.of(context).size.height / 2,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          order.length.toString() + " Items",
+                          style: normalFont(MColors.textGrey, 14.0),
+                        ),
+                        Spacer(),
+                        Text(
+                          "\$" + orderTotalPrice.toString(),
+                          style: boldFont(MColors.textGrey, 14.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 60.0,
+                          height: 25.0,
+                          child: RawMaterialButton(
+                            onPressed: () {
+                              _showModalSheet(orderListItem);
+                            },
+                            child: Text(
+                              "Details",
+                              style: boldFont(MColors.primaryPurple, 14.0),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          orderListItem.orderStatus,
+                          style: boldFont(Colors.green, 14.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
