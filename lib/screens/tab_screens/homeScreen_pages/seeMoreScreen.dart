872ca698e520a98328ada51d0de1852fdab8798f
Nifemi <thenifemi@gmail.com>
2020-09-02 22:10:52 -0300

    ..

--- lib/screens/tab_screens/homeScreen_pages/seeMoreScreen.dart
+++ lib/screens/tab_screens/homeScreen_pages/seeMoreScreen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mollet/model/data/Products.dart';
 
 class SeeMoreScreen extends StatefulWidget {
-  SeeMoreScreen({Key key}) : super(key: key);
+  final ProdProducts products;
+  SeeMoreScreen({Key key, this.products}) : super(key: key);
 
   @override
-  _SeeMoreScreenState createState() => _SeeMoreScreenState();
+  _SeeMoreScreenState createState() => _SeeMoreScreenState(products);
 }
 
 class _SeeMoreScreenState extends State<SeeMoreScreen> {
+  final ProdProducts products;
+  _SeeMoreScreenState(this.products);
+
   @override
   Widget build(BuildContext context) {
     return Container();
