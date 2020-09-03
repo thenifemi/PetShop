d9249460aa25be20985b51c1b25f34254dfbeebc
Nifemi <thenifemi@gmail.com>
2020-09-02 23:05:18 -0300

    ..

--- lib/screens/tab_screens/homeScreen_pages/seeMoreScreen.dart
+++ lib/screens/tab_screens/homeScreen_pages/seeMoreScreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mollet/model/data/Products.dart';
+import 'package:mollet/screens/tab_screens/search_screens/search_tabs.dart';
+import 'package:mollet/utils/colors.dart';
+import 'package:mollet/widgets/allWidgets.dart';
 
 class SeeMoreScreen extends StatefulWidget {
-  final ProdProducts products;
+  final List<ProdProducts> products;
   SeeMoreScreen({Key key, this.products}) : super(key: key);
 
   @override
@@ -10,11 +13,39 @@ class SeeMoreScreen extends StatefulWidget {
 }
 
 class _SeeMoreScreenState extends State<SeeMoreScreen> {
-  final ProdProducts products;
+  final List<ProdProducts> products;
   _SeeMoreScreenState(this.products);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: primaryAppBar(
+        IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: MColors.textGrey,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        Text(
+          "For you",
+          style: boldFont(MColors.primaryPurple, 16.0),
+        ),
+        MColors.primaryWhiteSmoke,
+        null,
+        true,
+        null,
+      ),
+      body: primaryContainer(
+        SearchTabWidget(
+          prods: dog,
+          cartNotifier: cartNotifier,
+          productsNotifier: productsNotifier,
+          cartProdID: cartProdID,
+        ),
+      ),
+    );
   }
 }
