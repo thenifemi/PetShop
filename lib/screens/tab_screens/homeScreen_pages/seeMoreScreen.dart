01a6132c5cb2ca7080b821bd81bbdb551ebaa7a4
Nifemi <thenifemi@gmail.com>
2020-09-02 22:01:18 -0300

    ..

---
+++ lib/screens/tab_screens/homeScreen_pages/seeMoreScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeeMoreScreen extends StatefulWidget {
+  SeeMoreScreen({Key key}) : super(key: key);
+
+  @override
+  _SeeMoreScreenState createState() => _SeeMoreScreenState();
+}
+
+class _SeeMoreScreenState extends State<SeeMoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
